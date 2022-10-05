fdd48dfb358db3e6cda1ab3f6f8d56636233ad08
RhiobeT <rhiobet@gmail.com>
2022-10-05 20:00:12 +0200

    Add post update script and service

---
+++ etc/systemd/system/post-update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -f /post-update-done ]; then
+    steamos-readonly disable
+    pacman-key --init
+    pacman-key --populate archlinux
+    pacman -S --noconfirm glibc
+    sed -i 's/#\(ja_JP.UTF-8.*\)/\1/' /etc/locale.gen
+    locale-gen
+    touch /post-update-done
+    steamos-readonly enable
+fi
