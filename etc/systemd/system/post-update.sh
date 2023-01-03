b8f90574d6f3d002a6b995d01076160c68b29d20
RhiobeT <rhiobet@gmail.com>
2023-01-03 15:38:02 +0100

    Install ranger

--- etc/systemd/system/post-update.sh
+++ etc/systemd/system/post-update.sh
@@ -4,7 +4,7 @@ if [ ! -f /post-update-done ]; then
     steamos-readonly disable
     pacman-key --init
     pacman-key --populate archlinux
-    pacman -S --noconfirm glibc
+    pacman -S --noconfirm glibc ranger
     sed -i 's/#\(ja_JP.UTF-8.*\)/\1/' /etc/locale.gen
     locale-gen
     touch /post-update-done
